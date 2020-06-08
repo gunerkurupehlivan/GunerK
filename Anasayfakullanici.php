eb7f07d2930cc3e89c09728bac227f0064f8ead3
gunerkurupehlivan <61183384+gunerkurupehlivan@users.noreply.github.com>
2020-06-08 11:50:39 +0300

    Add files via upload
    gunerkurupehlivan.com

--- Anasayfakullanici.php
+++ Anasayfakullanici.php
@@ -1,99 +1,73 @@
 <?php
-session_start();
+include 'gercek.php'
  ?>
  <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>teugetse.com</title>
-      <link rel="stylesheet" href="oldu.css">
+    <title>gunerkurupehlivan.com</title>
+      <link rel="stylesheet" href="ya.css">
   </head>
   <body>
-      <form class="" action="" method="post">
-    <div class="o">
 
-
-    <div class="bu">
-    <div class="giris">
-       <p></p><h1>Güner Kurupehlivan</h1>
-       <img src="IMG.jpg" alt="" weight="20px" height="150px">
-      <p></p><p>Bilgisayar Programcısı öğrencisi...</p>
-    </div><br><br>
-
-    <div class="ikinciekran">
-      <img src="e.png" alt="">
-      <div class="ikici">
-        <a href="Anasayfakullanici.php">Anasayfa</a>
-      </div>
-      <div class="ikici">
-        <a href="Hakkımdakullanici.php">Ben Kimim</a>
-      </div>
-      <div class="ikici">
-        <a href="Hikayemkullanici.php">Fotoğrafçılık</a>
-      </div>
-      <div class="ikici">
-       <a href="Makalekullanici.php">Makaleler</a>
-
-      </div>
-     <img src="e.png" alt="">
-    </div>
-    <div class="ücekren">
-      <br><br><img src="3.jpg" alt=" "weight="12px" height="188px" >
-    </div>
     <div class="dördüncü">
-      <h2>C# Hoşgeldin Dünya Kodları</h2>
-      <b>Console.Writline("Hoşgeldin Dünya")</b>
-      <br>
-      <h2> C# Klavyeden girilen 5 tane sayının en büyük ve en küçüğünü bulma</h2>
-      <h3>FOR DÖNGÜSÜ</h3>
-      <b>Console.Write("1. sayıyı girin:");<br>
-int sayi = Convert.ToInt32(Console.ReadLine());<br>
-int enKucuk = sayi;<br>
-int enBuyuk = sayi;<br>
-
-for(int i=1;i<10;i++)<br>
-{<br>
-   Console.Write("{0}. sayıyı girin:", i + 1);<br>
-   sayi = Convert.ToInt32(Console.ReadLine());<br>
-
-   if (sayi > enBuyuk)<br>
-      enBuyuk = sayi;<br>
-
-   if (sayi < enKucuk)<br>
-      enKucuk = sayi;<br>
-}<br>
-
-Console.WriteLine("Girilen en küçük sayı={0}", enKucuk);<br>
-Console.WriteLine("Girilen en büyük sayı={0}", enBuyuk);<br>
-Console.ReadKey();<br> </b>
-  <h3>WHİLE DÖNGÜSÜ</h3>
-  <b>Console.Write("1. sayıyı girin:");<br>
-int sayi = Convert.ToInt32(Console.ReadLine());<br>
-int enKucuk = sayi;<br>
-int enBuyuk = sayi;<br>
-int i = 1;<br>
-while (i < 10)<br>
-{<br>
-    Console.Write("{0}. sayıyı girin:", i + 1);<br>
-    sayi = Convert.ToInt32(Console.ReadLine());<br>
-
-    if (sayi > enBuyuk)<br>
-         enBuyuk = sayi;<br>
-
-    if (sayi < enKucuk)<br>
-         enKucuk = sayi;<br>
-
-    i++;<br>
-}<br>
-
-Console.WriteLine("Girilen en küçük sayı={0}", enKucuk);<br>
-Console.WriteLine("Girilen en büyük sayı={0}", enBuyuk);<br>
-Console.ReadKey();<br></b>
+  <section class="text-center">
+   <h3 class="baslık">Son Blog Yazıları </h3>
+    <div class="icerik">
+      <div class="icerikici">
+        <div class="foto">
+          <img src="cv.jpg" alt="" style="width:95%"  >
+          <div class="foto_ici">
+            <p class="baslık">Trakyam</p>
+            <p class="yazı">Ah korona ah nerden geldin sen.İnsan oğlu ateşi icat etmiş siz yarasayı hala çiğ yiyiyosunuz...</p>
+            <a href="yeni.php">Devamını oku >>></a>
+          </div>
+        </div>
+      </div>
+      <div class="icerikici">
+        <div class="foto">
+          <img src="bayram.jpg" alt="" style="width:95%">
+          <div class="foto_ici">
+            <p class="baslık">Trakyam</p>
+            <p class="yazı">Trakyada Bayramlar nasıl olur bilirmisiniz.Her bayram namazı çıkışı...</p>
+            <a href="yeni.php">Devamını oku >>></a>
+          </div>
+        </div>
+      </div>
+      <div class="icerikici">
+        <div class="foto">
+          <img src="genc.png" alt="" style="width:95%">
+          <div class="foto_ici">
+            <p class="baslık">Trakyam</p>
+            <p class="yazı">Trakyada gençlik yapmak diye bir muhabbet var.Toprak bastı adeti felan...</p>
+            <a href="yeni.php">Devamını oku >>></a>
+          </div>
+        </div>
+      </div>
+      <div class="icerikici">
+        <div class="foto">
+          <img src="tl.jpg" alt="" style="width:95%">
+          <div class="foto_ici">
+            <p class="baslık">Trakyam</p>
+            <p class="yazı">Bugün askerdenn gelen bi abi aradı.Kusura bakma askerdeyken arayamadım....</p>
+            <a href="yeni.php">Devamını oku >>></a><br><br><br><br><br><br>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
+
+
+  </div>
+
 
       </div>
       </div>
+
 
       </form>
   </body>
 </html>
+<?php
+include 'alt.php'
+ ?>
--- Hakkımdakullanici.php
+++ Hakkımdakullanici.php
@@ -1,58 +1,90 @@
+<?php
+include 'gercek.php'
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>teugetse.com</title>
-      <link rel="stylesheet" href="oldu.css">
-  </head>
-  <body>
-      <form  class="" action="baglan.php" method="post">
-    <div class="o">
+ <head>
+   <meta charset="utf-8">
+   <title>gunerkurupehlivan.com</title>
+     <link rel="stylesheet" href="ya.css">
+ </head>
+ <body>
+
+   <div class="bes">
+     <br><br><img src="gnr.jpg" alt=" "weight="16px" height="188px" > <br> <b>Güner Kurupehlivan-Computer Programmer</b><br>
+     <p>İlkokul ve ortaokulu Silivri'nin Büyükçavuşlu köyünde okudum.Lise'yi Silivri Necip Sarıbekir Mesleki Teknik ve Anadolu  Lisesinde okudum
+        ve Bilgi işlem bölümünden mezun oldum.
+     Lise stajımı Silivri belediyesi bilgi işlem müdürlüğünde yaptım.Şuan ise İstanbul Rumeli Üniversitesinde Bilgisayar Programcılığı okuyorum.
+     C#,C,HTML,PHP dillerine hakimim.Başlangıç düzeyinde ingilizcem var şuan fakat eğitimlerimi sürdürmekteyim.
+     Amacım DGS sınavıyla bilgisayar mühendisliğine geçmek.Kendime bir fotoğaf makinesi aldım ilk başta hobi olarak başladığım iş bir anda paraya
+   dönüşmeye başlamıştı.Köy düğünlerinde fotoğaf çekiyodum.</p>
+   </div>
+   <b>BLOG SAYFAMDA YAZAR OLMAK İÇİN AŞAĞIDAKİ FORMU DOLDURMAN YETERLİ </b><br>
+     <table>
+       <tr>
+         <td><label for="">Adınız:
+         </label></td>
+         <td> <input type="text" name="ad" value=""></td>
+       </tr>
+       <tr>
+         <td> <label for="">Soyadınız:
+         </label></td>
+         <td> <input type="text" name="s_ad" value=""></td>
+       </tr>
+       <tr>
+         <td> <label for=""> Şifre:</label></td><br>
+         <td> <input type="password" name="pass" value=""></td>
+             </tr>
+             <tr>
+               <td> <label for="">Telefon numarası:
+               </label></td>
+               <td> <input type="text" name="t_no" value=""></td>
+             </tr>
+
+               <td> <label for="">E-Posta:
+               </label></td>
+               <td> <input type="text" name="e_p" value=""></td>
+             </tr>
+
+       <tr>
+         <td> <label for="">Bir Kullanıcı Adı Belirleyin:
+         </label></td>
+         <td> <input type="text" name="k_ad" value=""></td>
+       </tr>
+       <tr>
+         <td> <br> </td>
+       </tr>
+
+         <tr>
+           <td><input type="submit" class="dördüncü" name="gonder" value="Kayıt Ol"></td>
+         </tr><br>
+       <tr>
+         <td> <br> </td>
+       </tr>
+
+ <tr>
+
+         <td> <button type="button" onClick="location.href='girisekranı.php'">Eğer Kayıt olduysanız giriş yapın.</td>
+
+       </tr>
 
 
-    <div class="bu">
-    <div class="giris">
-      <p></p><h1>Güner Kurupehlivan</h1>
-      <img src="IMG.jpg" alt="" weight="20px" height="150px">
-      <p></p><p>Bilgisayar Programcısı öğrencisi...</p>
-    </div><br><br>
-    <div class="ikinciekran">
-      <img src="e.png" alt="">
-      <div class="ikici">
-        <a href="Anasayfakullanici.php">Anasayfa</a>
-      </div>
-      <div class="ikici">
-        <a href="Hakkımdakullanici.php">Ben Kimim</a>
-      </div>
-      <div class="ikici">
-        <a href="Hikayemkullanici.php">Fotoğrafçılık</a>
-      </div>
-      <div class="ikici">
-        <a href="Makalekullanici.php">Makaleler</a>
-      </div>
-        <img src="e.png" alt="">
-    </div>
-    <div class="ücekren">
-      <br><br><img src="3.jpg" alt=" "weight="12px" height="188px" >
-    </div>
-    <div class="dördüncü">
-      <br>
-      <br>
-      <b>İlkokul ve ortaokulu Silivri'nin Büyükçavuşlu köyünde okudum.Lise'yi Silivri Necip Sarıbekir Mesleki Teknik ve Anadolu  Lisesinde okudum
-         ve Bilgi işlem bölümünden mezun oldum.
-      Lise stajımı Silivri belediyesi bilgi işlem müdürlüğünde yaptım.Şuan ise İstanbul Rumeli Üniversitesinde Bilgisayar Programcılığı okuyorum.
-      C#,C,HTML,PHP dillerine hakimim.Başlangıç düzeyinde ingilizcem var şuan fakat eğitimlerimi sürdürmekteyim.
-      Amacım DGS sınavıyla bilgisayar mühendisliğine geçmek.Kendime bir fotoğaf makinesi aldım ilk başta hobi olarak başladığım iş bir anda paraya
-    dönüşmeye başlamıştı.Köy düğünlerinde fotoğaf çekiyodum.  </b>
-    </div>
-    <br><br>
-  <div class="dördüncü">
-    <button type="button" onClick="location.href='ben.php'">Hakkımda Daha Falza..
-  </div>
-      </div>
-      </div>
 
 
-      </form>
-  </body>
+
+   </table>
+
+
+ </div>
+
+
+     </div>
+     </div>
+
+
+     </form>
+ </body>
 </html>
+<?php
+include 'alt.php'
+ ?>
